249d9e8f548f537a730455f3bec026bfb51e8bce
NadaHassan0 <hnada7678@gmail.com>
2024-03-02 16:26:24 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+# Read input
+numbers = list(map(int, input().split()))
+
+# Find minimum and maximum
+min_number = min(numbers)
+max_number = max(numbers)
+
+# Print the result
+print(min_number, max_number)
